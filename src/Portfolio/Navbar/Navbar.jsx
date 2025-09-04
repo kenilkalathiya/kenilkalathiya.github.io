558c2e5f6ec3fe54615173cc44f20fc39c02f8e8
Kenil Kalathiya <kenilkalathiya40@gmail.com>
2025-09-04 09:24:42 +0200

    Remove Extra Parts

--- src/Portfolio/Navbar/Navbar.jsx
+++ src/Portfolio/Navbar/Navbar.jsx
@@ -1,27 +1,3 @@
-// import React from "react";
-// import "./Navbar.css";
-
-// export default function Navbar() {
-//   return (
-//     <>
-//       <div className="navbar">
-//         <div className="portfolio">
-//           <h2>Portfolio</h2>
-//         </div>
-//         <div className="links">
-//           <ul className="navigation">
-//             <a href="#"><li>Home</li></a>
-//             <a href="#about-me"><li>About Me</li></a>
-//             <a href="#resume"><li>Resume</li></a>
-//             <a href="#"><li>Project</li></a>
-//             <a href="#"><li>Contact Me</li></a>
-//           </ul>
-//         </div>
-//       </div>
-//     </>
-//   );
-// }
-
 import React, { useState, useEffect } from "react";
 import "./Navbar.css";
 
